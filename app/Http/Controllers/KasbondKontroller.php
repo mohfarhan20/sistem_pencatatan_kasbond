1242227d5aba55231c5388e87c0a9c25cb0d4c28
mohfarhan20 <ghifarifarhan20@gmail.com>
2022-07-12 09:02:59 +0700

    fix paginate

--- app/Http/Controllers/KasbondKontroller.php
+++ app/Http/Controllers/KasbondKontroller.php
@@ -11,26 +11,27 @@ class KasbondKontroller extends Controller
 {
     public function index(Request $request)
     {
-// <<<<<<< HEAD
+        // <<<<<<< HEAD
         // $data_kasbond = DataKasbond::with('MasterData')->latest()->get();
-// =======
+        // =======
 
         $data_kasbond = DataKasbond::with('MasterData')->latest();
         if ($request->tanggal) {
             $data_kasbond = $data_kasbond->where('created_at', $request->tanggal);
         }
-        if(request('search')) {
+        if (request('search')) {
             $data_kasbond->where('keterangan_transaksi', 'like', '%' . request('search') . '%')
-                            ->orWhere('nominal', 'like', '%' . request('search') . '%');
+                ->orWhere('nominal', 'like', '%' . request('search') . '%');
         }
-        $data_kasbond = $data_kasbond->paginate(10);
+        $data_kasbond = $data_kasbond->paginate(5);
 
-// >>>>>>> 99cbdf6bd8971ccb9c018e882b32ffd5c4b8837b
+        // >>>>>>> 99cbdf6bd8971ccb9c018e882b32ffd5c4b8837b
         return view('index', compact('data_kasbond'));
     }
 
-    public function tambah()
+    public function tambah(Request $request)
     {
+        // $data_kasbond = DataKasbond::find($request->tanggal);
         $master_data = MasterData::all();
         return view('tambah', compact('master_data'));
     }
