c2fd5f7d68c36c4eceeb72946e40e9c392e8a38f
bintangkhd <bintangkhadafi2772@gmail.com>
2022-07-24 20:57:03 +0700

    add employee choosing unit room

---
+++ resources/views/home/employee_room.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Room</title>
+    <!-- Bootstrap 5.2 css cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- Added CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Poppins Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- Navbar -->
+    <section class="sticky-top">
+        <nav class="navbar navbar-expand-lg shadow">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="index.html">BSB BISA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item mx-3">
+                <a class="nav-link" aria-current="page" href="index.html">BERANDA</a>
+                </li>
+                <li class="nav-item mx-3 dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    FITUR
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">PESIRAH</a></li>
+                    <li><a class="dropdown-item" href="#">KREDIT USAHA RAKYAT(KUR)</a></li>
+                    <li><a class="dropdown-item" href="#">Delia</a></li>
+                    <li><a class="dropdown-item" href="#">EMPLOYEE ROOM</a></li>
+                </ul>
+                </li>
+                <li class="nav-item mx-3">
+                <a class="nav-link" href="#">TENTANG</a>
+                </li>
+                <li class="nav-item mx-3">
+                <a class="nav-link" href="#">KONTAK</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </section>
+    <!-- End Navbar -->   
+    
+    <!-- Choosing Card -->
+    <section class="choosing-card">
+        <div class="container-lg">
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <h1>EMPLOYEE ROOM</h1>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-5 px-2">
+                <div class="col-lg-2 text-center">
+                    <a href="unit_umum.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Umum dan Akuntansi</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                
+            </div>
+            <div class="row justify-content-center px-2">
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2 text-center">
+                    <a href="index.html">
+                        <div class="card-choosing mb-4">
+                            <div class="box-icon-choosing position-relative mx-auto">
+                            <!-- <img src="..." alt="Pesirah" class="position-absolute top-50 start-50 translate-middle"> -->
+                            </div>
+                            <p class="mt-3">Unit <br> Lain</p>
+                        </div>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!-- End Choosing Card -->
+
+        <!-- Footer -->
+    <section class="footer">
+        <div class="container-lg pt-5">
+        <div class="row">
+            <div class="col-lg-3 offset-lg-1">
+            <h1>BSB BISA</h1>
+            </div>
+            <div class="menu col-lg-3">
+                <ul>
+                <li><a href="index.html">Beranda</a></li>
+                <li><a href="index.html">Pesirah</a></li>
+                <li><a href="index.html">Kredit Usaha Rakyat (KUR)</a></li>
+                <li><a href="index.html">Kredit Serba Guna (KSG)</a></li>
+                <li><a href="index.html">Employee Room</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-4 address">
+            <h4 class="fw-bold">BANK SUMSEL BABEL 
+                CAB. KAPTEN A. RIVAI</h4>
+            <p>Jl. Kapten A. Rivai No.21, Sungai Pangeran, Kec. Ilir Tim. I, Kota Palembang, Sumatera Selatan 30129</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-12">
+            <p class="copyright text-center">COPYRIGHT 2022 BANK SUMSEL BABEL</p>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- End Footer -->
+
+    <!-- Bootstrap 5.2 js cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
